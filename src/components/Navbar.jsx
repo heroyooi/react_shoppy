d47ef7fd77a24c9d48752acceb8c0c015aea9414
heroyooi <heroyooi1018@gmail.com>
2023-07-20 17:16:10 +0900

    14.28 쿼리 제대로 사용하는 법 🎖

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,8 @@ import CartStatus from "./CartStatus";
 export default function Navbar() {
   const { user, login, logout } = useAuthContext();
 
+  console.log(user);
+
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-brand">
